a10941c58ee21c68c29aaa122fb3798523122361
Anthony Tan <>
2023-11-08 01:41:43 +0800

    add will dismiss callback

--- Agrume/AgrumeView.swift
+++ Agrume/AgrumeView.swift
@@ -10,6 +10,7 @@ public struct AgrumeView: View {
 
   private let images: [UIImage]
   private let startIndex: Int
+  var willDismiss: (() -> Void)? = nil
   @Binding private var binding: Bool
   @Namespace var namespace
 
@@ -24,7 +25,7 @@ public struct AgrumeView: View {
   }
 
   public var body: some View {
-    WrapperAgrumeView(images: images, isPresenting: $binding, startIndex: startIndex)
+      WrapperAgrumeView(images: images, isPresenting: $binding, startIndex: startIndex, willDimiss: self.willDismiss)
       .matchedGeometryEffect(id: "AgrumeView", in: namespace, properties: .frame, isSource: binding)
       .ignoresSafeArea()
   }
@@ -35,12 +36,14 @@ struct WrapperAgrumeView: UIViewControllerRepresentable {
 
   private let images: [UIImage]
   private let startIndex: Int
+    private var willDimiss: (() -> Void)? = nil
   @Binding private var binding: Bool
 
-    public init(images: [UIImage], isPresenting: Binding<Bool>, startIndex: Int = 0) {
+    public init(images: [UIImage], isPresenting: Binding<Bool>, startIndex: Int = 0, willDimiss: (() -> Void)?) {
     self.images = images
     self._binding = isPresenting
     self.startIndex = startIndex
+    self.willDimiss = willDimiss
   }
 
   public func makeUIViewController(context: UIViewControllerRepresentableContext<WrapperAgrumeView>) -> UIViewController {
@@ -50,6 +53,7 @@ struct WrapperAgrumeView: UIViewControllerRepresentable {
     agrume.addSubviews()
     agrume.addOverlayView()
     agrume.willDismiss = {
+        self.willDimiss?()
         binding = false
     }
     return agrume
